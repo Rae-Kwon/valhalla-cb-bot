dc277c7fb398771014585e9949f0a3e3bb8299bb
Richard Kwon <rkminjae@gmail.com>
2021-10-25 16:09:55 -0400

    remove unused functions

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,30 +1,8 @@
-const string = require('lodash/string')
-
-const getUsername = (message) => {
-    return message.author.username
-}
-
-const getMessage = (message) => {
-    return message.content 
-}
-
-const getClanBattleData = (message) => {
-    let [day, attack, score] = getMessage(message).split(',')
-
-    day = string.capitalize(day)
-    score = parseInt(score)
-
-    return [day, attack, score]
-}
-
 module.exports = {
     name: 'messageCreate',
     execute(message) {
         if (message.channel.name === 'attack-channel') {
-            messageContent = message
-            getUsername(message)
-            getClanBattleData(message)
-            console.log(`${getUsername(message)} sent ${getClanBattleData(message)}`)
+            console.log(`${message.member.displayName} sent ${message.content}`)
         }
     },
 }
