80bf259b4ba05b9b0e0f3aea4b454593c814c0d0
Richard Kwon <rkminjae@gmail.com>
2021-10-24 20:50:50 -0400

    add eventFiles and dynamically retrieve events

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { Client, Intents } = require('discord.js')
 const { token } = require('./config.json')
+const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'))
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.once('ready', () => {
-    console.log('Client is Ready!')
+eventFiles.forEach((file) => {
+    const event = require(`./events/${file}`)
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args))
+    } else {
+        client.on(event.name, (...args) => event.execute(...args))
+    }
 })
 
 client.login(token)
