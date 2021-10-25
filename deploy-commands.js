c41499e912ec0d346eb86690837fe436cd968fbc
Richard Kwon <rkminjae@gmail.com>
2021-10-24 21:55:50 -0400

    remove slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,16 @@
+const fs = require('fs')
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId, token } = require('./config.json')
 
-const commands = [
-    new SlashCommandBuilder().setName('record').setDescription('Records clan battle data to sheet'),
-    new SlashCommandBuilder().setName('test').setDescription('A test command to see if this works')
-].map((command) => command.toJSON())
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: '9' }).setToken(token)
 
