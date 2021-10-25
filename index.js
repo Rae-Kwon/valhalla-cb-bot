f9227f0b7515e291f6cccea2099dc4f07739b607
Richard Kwon <rkminjae@gmail.com>
2021-10-24 21:56:34 -0400

    remove commandFiles var

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const { token } = require('./config.json')
 const { Client, Collection, Intents } = require('discord.js')
 const eventFiles = fs.readdirSync('./events').filter((file) => file.endsWith('.js'))
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
