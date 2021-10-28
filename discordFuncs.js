a1105859b3ed93b1e33b7163b9cefbec932676f8
Richard Kwon <rkminjae@gmail.com>
2021-10-28 16:09:23 -0400

    add getIngameScore function

--- discordFuncs.js
+++ discordFuncs.js
@@ -9,18 +9,33 @@ const getMessage = (message) => {
 }
 
 const getClanBattleData = (message) => {
-    if (getMessage(message).length === 3) {
-        let [day, attack, score] = getMessage(message)
+    if (getMessage(message).length === 4) {
+        let [day, attack, score, clanBattle] = getMessage(message)
 
         day = parseInt(day.split('day')[1])
         attack = parseInt(attack)
         score = parseInt(score)
+        clanBattle = clanBattle.trim()
+        if (day > 5 || day < 1 || attack > 3 || attack < 1) {
+            throw 'The day or attack number is out of range'
+        }
 
-        return [day, attack, score]
+        return [day, attack, score, clanBattle]
+    }
+}
+
+const getIngameScore = (message) => {
+    if (getMessage(message).length === 3) {
+        let [ingame, score, clanBattle] = getMessage(message)
+
+        clanBattle = clanBattle.trim()
+
+        return [ingame, score, clanBattle]
     }
 }
 
 module.exports = {
     getUsername,
-    getClanBattleData
+    getClanBattleData,
+    getIngameScore
 }
