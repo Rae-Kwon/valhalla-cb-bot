4739947ef7d2ab57b960c18c27e9c917c24495c1
Richard Kwon <rkminjae@gmail.com>
2021-11-17 22:32:29 -0500

    change activities array to acitivty object

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ async function main() {
             bot.once(event.name, (...args) => event.execute(...args))
         } else {
             bot.on(event.name, (...args) => {
-                bot.user.setPresence({ activities:[{ name: 'Clan Battle', type: 'COMPETING' }], status: 'available' })
+                bot.user.setPresence({ 
+                    status: 'available', 
+                    activity: {
+                        name: 'Clan Battle', 
+                        type: 'COMPETING' 
+                    }
+                })
 
                 if (event.name === 'messageCreate') {
                     args.forEach((message) => {
