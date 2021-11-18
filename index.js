e4361b1d437651c1536385fc1272099c0ecbe66e
Richard Kwon <rkminjae@gmail.com>
2021-11-17 22:22:39 -0500

    add presence to say bot is competing in clan battle

--- index.js
+++ index.js
@@ -87,3 +87,4 @@ async function main() {
 
 main()
 client.login(process.env.DISCORD_TOKEN)
+client.user.setPresence({ activities:[{ name: 'in Clan Battle', type: 'COMPETING' }], status: 'available' })
